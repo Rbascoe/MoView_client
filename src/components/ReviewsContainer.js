5d00ef63faf2ce98e2499f2edf27bf18a180292a
Rbascoe <roanbascoe5@gmail.com>
2020-10-19 09:14:17 -0400

    Reviews show on movie show page

--- src/components/ReviewsContainer.js
+++ src/components/ReviewsContainer.js
@@ -4,10 +4,10 @@ const ReviewsContainer = (props) => {
     console.log(props)
     return (
         <div>
-            <h2>Reviews Go Here</h2>
-            
-            {props.reviews.map(review => review.content)}
-           
+            <h2>Reviews</h2>
+            <ul>
+            {props.reviews.map(review => <li>{review.content}</li>)}
+            </ul>
         </div>
     )
 }
