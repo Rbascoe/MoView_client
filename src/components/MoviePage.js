86419566cb4e30288037deadfadc0f5a3a91c5dd
Rbascoe <roanbascoe5@gmail.com>
2020-10-21 14:27:05 -0400

    fixed moviepage grid appearance

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -28,10 +28,10 @@ export const MoviePage = (props) => {
 
 
     return (
+        <Container>
         <div>
-            <Container>
                 <Grid container spacing={0} >
-                <Grid item className={classes.container}>
+                <Grid item xs={6}>
                     <img src={`http://image.tmdb.org/t/p/w200/${props.poster}`} />
                     <h3>Title:</h3><br></br>
                     <b>{props.title}</b>
@@ -42,7 +42,7 @@ export const MoviePage = (props) => {
                     <h3>Release date: </h3><br></br>
                     <b>{props.release_date}</b>
                 </Grid>
-                <Grid item className={classes.container}> 
+                <Grid item xs={6}> 
                     <div>
                     {localStorage.id?
                     <ReviewForm id={props.id} />: null}
@@ -52,8 +52,8 @@ export const MoviePage = (props) => {
                     </div>
                 </Grid>
                 </Grid>
-            </Container>
         </div>
+        </Container>
     )
 }
 
--- src/components/ReviewsContainer.js
+++ src/components/ReviewsContainer.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+      backgroundColor: '#e53935',
+      contrastText: 'white',
+    //   align: 'right'
+    },
+        
+  }));
+  
+  
 const ReviewsContainer = (props) => {
-    console.log(props)
+    // console.log(props)
+    const classes = useStyles();
     return (
         <div>
             <h2>Reviews</h2>
             <ul>
-                {props.reviews.map(review => <li>{review.content}<br></br>-{localStorage.username}</li>)}
+                {props.reviews.map(review => <li><b>{review.content}</b><br></br>
+                <Button className={classes.button}>Edit</Button>
+                <Button className={classes.button}>Delete</Button></li>)}
             </ul>
         </div>
     )
